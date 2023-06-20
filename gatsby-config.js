f9be1910df3ef4833d417aed13765ceaf55369aa
cuonghx-gutech <cuonghx@gu-tech.com>
2023-06-20 22:16:26 +0700

    feat: add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,5 +112,12 @@ module.exports = {
         icon: `src/images/avatar.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "G-39ZQZ09VWV",
+        anonymize: true,
+      },
+    },
   ],
 }
